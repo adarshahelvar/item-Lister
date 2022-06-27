1e7c642b01c3808f08d0202e2817d31aa529a4a4
Adarsha A <adarshaadi1997@gmail.com>
2022-06-27 15:45:54 +0530

    dom manupilation

---
+++ dom.js
@@ -0,0 +1,19 @@
+// console.dir(document);
+// // console.log(document.getElementById('header-title'));
+// var headTitle=document.getElementById('header-title');
+// var header = document.getElementById('main-header');
+
+// headTitle.textContent='Nane Rajakumara';
+
+// headTitle.style.borderBottom='solid 5px #000';
+// header.style.borderBottom='solid 5px #000';
+
+
+
+var headerTitle= document.getElementById('header-title');
+headerTitle.style.borderBottom='solid 5px #000';
+var header=document.getElementById('main-header');
+header.style.borderBottom='solid 5px red';
+var itemList=document.getElementById('items');
+itemList.style.color='green';
+itemList.style.fontWeight='900';
